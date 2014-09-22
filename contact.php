c3fce1fca96ac757ec42c70ccf8443c9ad00ef29
Sean Brown <inphomercial@gmail.com>
2014-09-22 17:05:49 -0600

    updated email address and subject for contact form

--- contact.php
+++ contact.php
@@ -7,21 +7,19 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
- $to = 'inphomercial@gmail.com';
- $subject = 'Coffeepod Form Message';
+ $to = 'provocoffeepod@gmail.com';
+ $subject = 'Coffeepod Site Message';
  $message = 'Message: '.$message;
- $headers = 'From: '.$email. "\r\n";
+ $headers = 'From: '.$email;
 
   if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // this line checks that we have a valid email address
 	mail($to, $subject, $message, $headers); //This method sends the mail.
-	$response['status'] = 1;	
+	$response['status'] = 1;
 	$response['html'] = "Thank you!";
 	echo json_encode($response);
-  	// echo "Your email was sent!"; // success message
   }else{
 	$response['html'] = "Invalid Email";
 	$response['status'] = 0;
 	echo json_encode($response);
-  	// echo "Invalid Email, please provide an correct email.";
   }
 // ?>
